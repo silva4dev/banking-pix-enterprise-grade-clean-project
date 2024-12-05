e6c36d75e3c5eb69bca0cc4af31a043d89873ace
silva4dev <web4dev.lucas@outlook.com>
2024-12-04 21:36:20 -0300

    test: :test_tube: add pix key model

---
+++ codepix/domain/model/pixKey_test.go
@@ -0,0 +1,35 @@
+package model_test
+
+import (
+	"testing"
+
+	uuid "github.com/satori/go.uuid"
+	"github.com/silva4dev/microservices-codepix-project/domain/model"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestModel_NewPixKey(t *testing.T) {
+	code := "001"
+	name := "Banco do Brasil"
+	bank, _ := model.NewBank(code, name)
+
+	accountNumber := "abcnumber"
+	ownerName := "Lucas"
+	account, _ := model.NewAccount(bank, accountNumber, ownerName)
+
+	kind := "email"
+	key := "j@j.com"
+	pixKey, err := model.NewPixKey(kind, account, key)
+
+	require.NotEmpty(t, uuid.FromStringOrNil(pixKey.ID))
+	require.Equal(t, pixKey.Kind, kind)
+	require.Equal(t, pixKey.Status, "active")
+
+	kind = "cpf"
+	_, err = model.NewPixKey(kind, account, key)
+	require.Nil(t, err)
+
+	_, err = model.NewPixKey("nome", account, key)
+	require.NotNil(t, err)
+}
