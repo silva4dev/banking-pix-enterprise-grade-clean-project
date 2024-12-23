86921527397e7138d682e9d51e5911356e3bfae8
silva4dev <web4dev.lucas@outlook.com>
2024-12-23 17:28:24 -0300

    chore: :sparkles: add kafka cli

---
+++ codepix/cmd/kafka.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"os"
+
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/silva4dev/microservices-codepix-project/application/kafka"
+	"github.com/silva4dev/microservices-codepix-project/infrastructure/db"
+	"github.com/spf13/cobra"
+)
+
+var kafkaCmd = &cobra.Command{
+	Use:   "kafka",
+	Short: "Start consuming transactions using Apache Kafka",
+	Run: func(cmd *cobra.Command, args []string) {
+		deliveryChan := make(chan ckafka.Event)
+		database := db.ConnectDB(os.Getenv("env"))
+		producer := kafka.NewKafkaProducer()
+		go kafka.DeliveryReport(deliveryChan)
+		kafkaProcessor := kafka.NewKafkaProcessor(database, producer, deliveryChan)
+		kafkaProcessor.Consume()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(kafkaCmd)
+}
